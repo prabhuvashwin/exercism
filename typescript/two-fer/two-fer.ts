7804462dcc36089779db8311afb37856acf7f29a
prabhuvashwin <ashwinvprabhu@icloud.com>
2023-01-15 22:08:05 -0800

    updated two-fer solution

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -2,7 +2,7 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(name?: string): string {
+export function twoFer(name: string = 'you'): string {
   // ^                 ^   ^ this is called a return type; it's the type of the
   // ^                 ^     value that is returned from this function
   // ^                 ^
@@ -11,5 +11,5 @@ export function twoFer(name?: string): string {
   // allows the tests to import this function and call it
   // <-- Your code goes here. You may remove all the commentary in this file.
 
-  return `One for ${name ? name : 'you'}, one for me.`;
+  return `One for ${name}, one for me.`;
 }
